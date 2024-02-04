dc7223e3f67b4352ee2fd8c866b465c3e491b2fd
timwitzdam <tim2013w@gmail.com>
2024-02-04 19:47:42 +0100

    Moved all tools into categories

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
@@ -8,6 +8,17 @@ export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
+  redirects: {
+    "/tools/png-to-webp": "/tools/convert/image/png-to-webp",
+    "/tools/jpg-to-webp": "/tools/convert/image/jpg-to-webp",
+    "/tools/jpeg-to-webp": "/tools/convert/image/jpeg-to-webp",
+    "/tools/webp-to-png": "/tools/convert/image/webp-to-png",
+    "/tools/bin-to-hex": "/tools/convert/number/bin-to-hex",
+    "/tools/hex-to-bin": "/tools/convert/number/hex-to-bin",
+    "/tools/website-word-counter": "/tools/web/seo/website-word-counter",
+    "/tools/random-id-generator": "/tools/coding/generator/random-id-generator",
+    "/tools/js-beautifier": "/tools/coding/formatter/js-beautifier",
+  },
 });
--- src/glob_vars.ts
+++ src/glob_vars.ts
@@ -11,19 +11,48 @@ interface ToolCategory {
 
 export const tool_categories: ToolCategory[] = [
   {
-    name: "Converters",
-    description: "Converters",
-    slug: "converters",
+    name: "Converter",
+    description: "Converter",
+    slug: "convert",
     sub_categories: [
       {
         name: "Numbers",
         description: "Number Converters",
-        slug: "number-converters",
+        slug: "number",
       },
       {
         name: "Images",
         description: "Image Converters",
-        slug: "image-converters",
+        slug: "image",
+      },
+    ],
+  },
+  {
+    name: "Web",
+    description: "Web",
+    slug: "web",
+    sub_categories: [
+      {
+        name: "SEO",
+        description: "SEO Tools",
+        slug: "seo",
+      },
+    ],
+  },
+  {
+    name: "Coding",
+    description: "Coding",
+    slug: "coding",
+    sub_categories: [
+      {
+        name: "Formatter",
+        description: "Formatter",
+        slug: "formatter",
+      },
+      {
+        name: "Generator",
+        description: "Generator",
+        slug: "generator",
       },
     ],
   },

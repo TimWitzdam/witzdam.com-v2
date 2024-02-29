f4bc4301aa57d69fff420d28e5d217abad8e0466
timwitzdam <tim2013w@gmail.com>
2024-02-29 16:32:49 +0100

    Added asto starlight documentation template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,33 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(),
+    react(),
+    starlight({
+      title: "Tim's API Documentation",
+      disable404Route: true,
+      sidebar: [
+        {
+          label: "Start here",
+          // link: "/",
+          items: [
+            { label: "Getting started", link: "/docs" },
+            { label: "Usage limits", link: "/docs/usage-limits" },
+          ],
+        },
+      ],
+    }),
+  ],
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
   redirects: {
     "/tools/png-to-webp": "/tools/convert/image/png-to-webp",
@@ -20,6 +38,6 @@ export default defineConfig({
     "/tools/hex-to-bin": "/tools/convert/number/hex-to-bin",
     "/tools/website-word-counter": "/tools/web/seo/website-word-counter",
     "/tools/random-id-generator": "/tools/coding/generator/random-id-generator",
-    "/tools/js-beautifier": "/tools/coding/formatter/js-beautifier"
-  }
+    "/tools/js-beautifier": "/tools/coding/formatter/js-beautifier",
+  },
 });
---
+++ src/content/docs/docs/index.md
@@ -0,0 +1,14 @@
+---
+title: Getting started
+description: Learn how to use any API from Tim's API collection
+hero:
+  tagline: Here you can learn how to use any API from Tim's API collection.
+  actions:
+    - text: Explore APIs
+      link: /guides/example/
+      icon: right-arrow
+      variant: primary
+    - text: Tim's Tools
+      link: /tools
+      icon: external
+---
